2d752f1fa636af6b996320c9ea4f3993b9607e10
BhrtAgrwl01 <f20190725@pilani.bits-pilani.ac.in>
2021-07-18 20:17:14 +0000

    updated quickstart tensorflow's server to configure 3 clients

--- examples/quickstart_tensorflow/server.py
+++ examples/quickstart_tensorflow/server.py
@@ -1,5 +1,14 @@
 import flwr as fl
+import sys
 
 # Start Flower server for three rounds of federated learning
 if __name__ == "__main__":
-    fl.server.start_server("127.0.0.1:12345", config={"num_rounds": 3})
+    strategy = fl.server.strategy.FedAvg(
+        fraction_fit=1,
+        fraction_eval=1,
+        min_fit_clients=3,
+        min_eval_clients=3,
+        min_available_clients=3,
+    )
+    for i in range(int(sys.argv[1])):
+        fl.server.start_server("127.0.0.1:12345", config={"num_rounds": 10}, strategy=strategy)
